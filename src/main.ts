2f121e4087f28867c7480186c497ebedbd3505ce
OsamaImadi <osamaimadi@yahoo.com>
2022-12-28 05:39:40 +0500

    cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors()
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'https://rococo-kataifi-947ff0.netlify.app/',
+    credentials: true,
+  })
   const port = process.env.PORT || 3001
   await app.listen(port);
 }
